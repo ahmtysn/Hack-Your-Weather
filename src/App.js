bece7ba567b7605ebd88a52fb94275c8607eabef
ahmtysn <ahmtysn14@gmail.com>
2020-04-16 20:53:00 +0200

    modified

--- src/App.js
+++ src/App.js
@@ -1,27 +1,13 @@
 import React from "react";
 import "./App.css";
-import CityForm from "./components/CityForm";
-import CityWeather from "./city-weather.json";
+import Header from "./components/Header";
+import Main from "./components/Main";
 
-function App() {
+export default function App() {
   return (
     <div className="App">
-      <h1>Weather</h1>
-      {CityWeather.map((city) => (
-        <CityForm
-          key={city.id}
-          cityName={city.name}
-          countryName={city.sys.country}
-          main={city.weather[0].main}
-          description={city.weather[0].description}
-          temp_min={Math.floor((city.main.temp_min - 273) * 10) / 10}
-          temp_max={Math.floor((city.main.temp_max - 273) * 10) / 10}
-          lon={city.coord.lon}
-          lat={city.coord.lat}
-        />
-      ))}
+      <Header />
+      <Main />
     </div>
   );
 }
-
-export default App;
